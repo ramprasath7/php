4d1aa470b56d4deca5f10cdbac9a0f395b14d83d
root <root@ip-172-31-71-201.ec2.internal>
2021-06-29 03:06:22 +0000

    DCC in

--- index.php
+++ index.php
@@ -9,12 +9,12 @@
 		include_once('assets/includes/header.php');
 ?>
 <!-- START PAGE CONTENT -->
-<h1>-----------Master Branch---------</h1>
+<h1>-----------ramprasath.j---------</h1>
 <h1>Testtt</h1>
 <p>
 You’ll find EWU in Chicago’s South Loop neighborhood, easily accessible by bus, train and subway. Your professors are experienced professionals, experts in their fields with the ability and willingness to provide the guidance you need to succeed. You’ll also find a financial aid department dedicated to helping you – through part-time employment, grants, scholarships, loans and more – pursue every available means to attend.
 <br/><br/>
-EWU is one of the most cost-effective education institutions in the regions – coming in 5th for the lowest average graduate student debt in a survey of more than 1000 public and private colleges by the New York Times
+அமெரிக்காவுக்கு சென்று சிகிச்சை மேற்கொள்ள திட்டமிட்ட ரஜினி, இதற்காக மத்திய அரசிடம் அனுமதி கேட்டிருந்தார். அனுமதி கிடைத்த நிலையில், கடந்த வாரம் ரஜினிக்கு சிகிச்சை நல்லபடியாக முடிந்துள்ளது.EWU is one of the most cost-effective education institutions in the regions – coming in 5th for the lowest average graduate student debt in a survey of more than 1000 public and private colleges by the New York Times
 </p>
 
 
